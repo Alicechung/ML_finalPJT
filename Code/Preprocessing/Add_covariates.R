630f74da1bd2a76ed829858b663313c4fec97869
minjukim1220 <35040116+minjukim1220@users.noreply.github.com>
2018-02-26 03:51:04 -0600

    R code and revised final dataset
    added covariates on state-level unemployment, status as incumbent vs. challenging party, party of local governor

---
+++ Code/Preprocessing/Add_covariates.R
@@ -0,0 +1,66 @@
+rm(list=ls())
+
+library(readr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+library(ggmap)
+library(magrittr)
+library(MASS)
+library(dplyr)
+library(stargazer)
+library(plm)
+library(zeligverse)
+library(ggplot2)
+library(pscl)
+library(arm)
+library(fBasics, warn.conflicts = FALSE)
+library(foreign)
+library(readtext)
+library(stringr)
+library(stm)
+library(stats)
+library(gpclib)
+library(gridExtra)
+
+select <- dplyr::select 
+sim <- Zelig::sim
+logit <- VGAM::logit
+expand <- tidyr::expand
+case_when <- dplyr::case_when
+
+setwd("C:/Users/minju/Dropbox/UChicago/MachineLearning/FinalProject")
+mac_final <- read.csv("C:/Users/minju/Documents/GitHub/ML_finalPJT/Data/alldf_180223.csv", header=TRUE)
+
+###Add state-level unemployment
+unemploy <- read.csv("C:/Users/minju/Dropbox/UChicago/MA Thesis/TAA/TAAcases/unemployment_statelevel.csv", header=TRUE)
+mac_final <- left_join(mac_final, unemploy, by = c("Year" = "year", "Abb" = "state"))
+
+###position as incumbent party vs. challenging party
+mac_final <- mac_final%>%
+  mutate(challenger = ifelse( Year == 2008 & PartyID == "Dem"|
+                                Year == 2012 & PartyID == "Rep"|
+                                Year == 2016 & PartyID == "Rep", 1, 0))
+
+###local governor party similarity
+localg <- read.csv("C:/Users/minju/Dropbox/UChicago/MA Thesis/TAA/TAAcases/states_partystrength.csv", na.strings = "", header=TRUE)
+lgparty <- localg%>%select(state, year, governor)%>%dplyr::filter(year > 2007)
+##filling in north carolina blanks
+lgparty <- lgparty%>%mutate(nc = case_when((state=="north carolina"& year > 2007) &(state=="north carolina"& year < 2013) ~ "D", 
+                                           (state=="north carolina"& year > 2012) ~ "R"))
+lgparty$governor <- as.character(lgparty$governor)
+lgparty$nc <- as.character(lgparty$nc)
+lgparty <- lgparty%>%mutate(governor2 = ifelse(is.na(governor), nc, governor))%>%
+  select(-governor, -nc)
+
+lgparty <- lgparty%>%mutate(governorp = case_when(governor2 == "D" ~ "Dem", 
+                                                governor2 == "R" ~ "Rep",
+                                                governor2 == "I" ~ "Independent",
+                                                governor2 == "Mark Dayton (DFL)" ~ "Ohters"))
+lgparty <- lgparty%>%select(-governor2)
+
+##left_join
+mac_final$State = tolower(mac_final$State)
+mac_final <- left_join(mac_final, lgparty, by = c("Year" = "year", "State" = "state"))
+
+sum(length(which(!is.na(mac_final$swing_last_08) & is.na(mac_final$governorp))))
